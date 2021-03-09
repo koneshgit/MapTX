3508d2a8777a3cdd7e66a2167f159320b524bec5
Mo Koneshloo <mkoneshloo@gmail.com>
2021-03-09 12:41:35 -0600

    Added: Pan, Zoom, Select/ Factorizing codeforfuture developement

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # MapTX
 This repository contains the code and some example data to generate a map visualization of data. It can be used to visualize any quantitive variable as a series of polygon on map. To this aim two components are requiered:
 
-1. Appropriate file of the geometry: a GeoJson file continin a GEOID key (it can be zip code, county subdivision code, county code, ....)
-2. Dataset with the mesured value on each polygone and the same GEOID key  
+1-Appropriate file of the geometry: a GeoJson file continin a GEOID key (it can be zip code, county subdivision code, county code, ....)
+2- Dataset with the mesured value on each polygone and the same GEOID key  
 
-## bokeh_mapper
-This code reads these two file, preprocess and merge them to generate Bokeh's input. 
+## bokeh_mapper.py
+This script reads these two file, preprocess and merge them to generate Bokeh's input. 
 Bokeh provoides a set of flexible tools to interact with visulized map, which is generated as an HTML file by the code. 
 All setings come from a json file, explined in the next section. The only parameter of the code is the address to this json file.
 It is hardcoded for now.
@@ -15,51 +15,46 @@ At this step, code reads the parameter from a json file, described below.
 The list of parameter consists of several parts; input, visual, and output parameters
 
 ### Input Parmeters:
-* source to read polygons' boundry
-	`"shapeFile":"../data/TractPolygon.geojson",`
+* Source to read polygons' boundry
+"shapeFile":"../data/TractPolygon.geojson",
 * source to read file from an Url address:
-	`"shapeUrl":"None",`
+"shapeUrl":"None",
 * Source to read data set, in the future version these can be replaced with result of a SQL query
-	`dataFile":"../data/TractData.csv",`
+"dataFile":"../data/TractData.csv",
 * Subset fields of the geometry file to reduce the file volume/remove unnecessery data, dict format
-```
-	"shapeFields":{
-		"s1":"GEOID",
-		"s2":"geometry"
+"shapeFields":{
+	"s1":"GEOID",
+	"s2":"geometry"
 	},
-```
 * Link key from geometry file
-	`shapeKey":"GEOID",
+"shapeKey":"GEOID",
 * Subset fields of the dataset to reduce the file volume/remove unnecessery data, dict format
-```
-	"dataFields":{
-		"d1":"County",
-		"d2":"GEOID",
-		"d3":"Area",
-		"d4":"Population"
+"dataFields":{
+	"d1":"County",
+	"d2":"GEOID",
+	"d3":"Area",
+	"d4":"Population"
 	},
-```
 * Link key from geometry file
-	`"dataKey":"GEOID",`
+"dataKey":"GEOID",
 * Main parameter to be used as color scale
-	`"data2Viz":"Population",`
+"data2Viz":"Population",
 
 ### Visual setting
 * Proper presentation of variable name on map
-	`"dataName2Vize":"Census estimated population",`
+"dataName2Vize":"Census estimated population",
 * Colormap
-	`"cMapName": "YlGnBu",`
+"cMapName": "YlGnBu",
 * Number of color (not in use yet)
-	`"cNumber" : "32",`
+"cNumber" : "32",
 * Using reversed color pallet
-	`"reversedColorMap" : "False",`
+"reversedColorMap" : "False",
 * Map title
-	`"figTitle" : "Census Data Map",`
+"figTitle" : "Census Data Map",
 * TipTools shows the GEOID and main parameter when you hover on each polygon, you can add a theird one, by adding Field name (starting by @), and its title to show in tiptoll
-```
-	"tipTitle": "Area",
-	"tipName": "@Area",
-```
+"tipTitle": "Area",
+"tipName": "@Area",
+
 ### Output 
-* Path and name of output
-	`"htmlOut": "../data/TractOut.html",`
+"htmlOut": "../data/TractOut.html",
+}
--- pycode/bokeh_mapper.py
+++ pycode/bokeh_mapper.py
@@ -17,130 +17,107 @@ from bokeh.plotting import figure, output_file, save
 # Setting parameters ------------------------------------------------
 #input params
 settingSrc = r"../data/TractSetting.json"
+#-------------------------------------------------------------------
 def load_settings(settingSrc):
     with open(settingSrc) as f:
         pSet = json.load(f)
         pSet["shapeFields"]=list(pSet["shapeFields"].values())
         pSet["dataFields"]=list(pSet["dataFields"].values())
-
     return pSet
+
+def load_dataSet(pSet): 
+    df = pd.read_csv(pSet["dataFile"])
+    if len(pSet["dataFields"])>0:
+        df = df[pSet["dataFields"]]
+    return df
+
+def load_polygon(pSet): 
+    _, file_extension = path.splitext(pSet["shapeFile"])    
+    if file_extension.lower() =='geojson':
+        gdf = gpd.read_file(open(pSet["shapeFile"]))
+    else:
+        gdf = gpd.read_file(pSet["shapeFile"])
+        
+    if len(pSet["shapeFields"])>0:
+        gdf = gdf[pSet["shapeFields"]]         
+
+    return gdf 
     
-#Output params
-# cMapName = 'YlGnBu'
-# cNumber = 32
-# reversedColorMap = 0
-# figTitle = ' Visualization map'
+def geo_merge(pSet,df,gdf):
+    
+    #gdf.info()
+    gdf[pSet["dataKey"]]=gdf[pSet["shapeKey"]].astype('int64')
+    
+    merged = gdf.merge(df, left_on = pSet["dataKey"], right_on = pSet["dataKey"], how = 'left')  
+    merged = merged.dropna()
 
-# Check input   -------------------------------------------------------
+    merged_json = json.loads(merged.to_json())
+    json_data = json.dumps(merged_json)
+    # with open("merged.json", "w") as data_file:
+    #     json.dump(merged_json, data_file, indent=2)
 
-# Files are accesible
-pSet = load_settings(settingSrc)
+    return GeoJSONDataSource(geojson = json_data)
 
-# Fields are in dataframes
-_, file_extension = path.splitext(pSet["shapeFile"])
+def cMapper (pSet):
+    #Define a sequential multi-hue color palette.
+    # Create control on min and max , merged is requierd for colorMapper Function low = 0, high = 40)
+    """
+    adding colormapper option
+    bokeh.models.mappers.ColorMapper (Python class, in bokeh.models.mappers)
+    bokeh.models.mappers.CategoricalColorMapper (Python class, in bokeh.models.mappers)
+    bokeh.models.mappers.ContinuousColorMapper (Python class, in bokeh.models.mappers)
+    bokeh.models.mappers.EqHistColorMapper (Python class, in bokeh.models.mappers)
+    bokeh.models.mappers.LinearColorMapper (Python class, in bokeh.models.mappers)
+    bokeh.models.mappers.LogColorMapper (Python class, in bokeh.models.mappers)
+    """
+    
+    palette = brewer[pSet["cMapName"]][8] #[cNumber]
+    if pSet["reversedColorMap"]:
+        palette = palette[::-1]
+    
+    return LinearColorMapper(palette = palette)
 
-if file_extension.lower() =='geojson':
-    gdf = gpd.read_file(open(pSet["shapeFile"]))
-else:
-    gdf = gpd.read_file(pSet["shapeFile"])
-
-gdf=gdf[pSet["shapeFields"]]
-if len(pSet["shapeFields"])>0:
-    gdf = gdf[pSet["shapeFields"]] 
-
-#gdf.info()
-gdf[pSet["dataKey"]]=gdf[pSet["shapeKey"]].astype('int64')
-
-df = pd.read_csv(pSet["dataFile"])
-if len(pSet["dataFields"])>0:
-    df = df[pSet["dataFields"]]
-#df.head()
-
-
-# Keys are in dataframes
-
-
-#--------------------------------------------------------------------
-#Perform left merge to preserve every row in gdf.
-merged = gdf.merge(df, left_on = pSet["dataKey"], right_on = pSet["dataKey"], how = 'left')
-
-#Replace NaN values
-merged = merged.dropna()
-
-#Read data to json
-merged_json = json.loads(merged.to_json())
-
-#Convert to str like object
-json_data = json.dumps(merged_json)
-# with open("merged.json", "w") as data_file:
-#     json.dump(merged_json, data_file, indent=2)
-
-#Input GeoJSON source that contains features for plotting.
-geosource = GeoJSONDataSource(geojson = json_data)
-
-#Define a sequential multi-hue color palette.
-palette = brewer[pSet["cMapName"]][8] #[cNumber]
-
-#Reverse color order so that dark blue is highest obesity.
-if pSet["reversedColorMap"]:
-    palette = palette[::-1]
-
-#Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
-color_mapper = LinearColorMapper(palette = palette)#, low = 0, high = 40)
-
-"""
-adding colormapper option
-bokeh.models.mappers.ColorMapper (Python class, in bokeh.models.mappers)
-bokeh.models.mappers.CategoricalColorMapper (Python class, in bokeh.models.mappers)
-bokeh.models.mappers.ContinuousColorMapper (Python class, in bokeh.models.mappers)
-bokeh.models.mappers.EqHistColorMapper (Python class, in bokeh.models.mappers)
-bokeh.models.mappers.LinearColorMapper (Python class, in bokeh.models.mappers)
-bokeh.models.mappers.LogColorMapper (Python class, in bokeh.models.mappers)
-"""
-
-
-
-#Add later custom tick labels for color bar.
-#tick_labels = {'0': '0%', '5': '5%', '10':'10%', '15':'15%', '20':'20%', '25':'25%', '30':'30%','35':'35%', '40': '>40%'}
-
-#Add hover tool
-hover = HoverTool(tooltips = [ ("ID","@"+pSet["dataKey"]),
+def add_handle(pSet):
+        #Add hover tool
+        hover = HoverTool(tooltips = [ ("ID","@"+pSet["dataKey"]),
                               (pSet["dataName2Vize"], "@"+pSet["data2Viz"]),
                               (pSet["tipTitle"],pSet["tipName"])])
-
-#Create color bar. 
-color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8,width = 500, height = 20,
-border_line_color=None,location = (0,0), orientation = 'horizontal') #, major_label_overrides = tick_labels)
-
-#Create figure object.
-p = figure(title = pSet["figTitle"], plot_height = 600 , plot_width = 950, 
-           toolbar_location = 'left',tools=[hover])
-p.xgrid.grid_line_color = None
-p.ygrid.grid_line_color = None
-
-#Add patch renderer to figure. 
-p.patches('xs','ys', source = geosource,fill_color = {'field' :pSet["data2Viz"],'transform' : color_mapper},
-          line_color = 'black', line_width = 0.25, fill_alpha = 1)
-
-#Specify figure layout.
-p.add_layout(color_bar, 'below')
-
-#Legend
-p.legend.location = "top_left"
-p.legend.click_policy="hide"
-
-#hist = Histogram(df[pSet['data2Viz']], title=pSet['data2Viz'])
+        return [hover, 'pan', 'tap', 'wheel_zoom']
+    
+def fig_gen(pSet, color_mapper, map_handle,geosource):
+    #Create color bar. 
+    color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8,width = 500, height = 20,
+    border_line_color=None,location = (0,0), orientation = 'horizontal') #, major_label_overrides = tick_labels)
+    
+    #Create figure object.
+    p = figure(title = pSet["figTitle"], plot_height = 600 , plot_width = 950, 
+               toolbar_location = 'left',tools = map_handle)
+    
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    
+    p.patches('xs','ys', source = geosource,fill_color = {'field' :pSet["data2Viz"],'transform' : color_mapper},
+              line_color = 'black', line_width = 0.25, fill_alpha = 1)
+    
+    #Specify figure layout.
+    p.add_layout(color_bar, 'below')
+    
+    return p
 
 
-output_file(filename=pSet["htmlOut"], title="Static HTML file")
-#save(hplot(p, hist))
-save(p,filename = pSet["htmlOut"])
+    
+def main_mapper(settingSrc): 
+# Files are accesible
+    pSet = load_settings(settingSrc)
+    # Fields are in dataframes
+    df = load_dataSet(pSet)
+    gdf = load_polygon(pSet)
+    geosource = geo_merge(pSet,df,gdf)
+    color_mapper = cMapper (pSet)
+    map_handle = add_handle(pSet)
+    p =fig_gen(pSet, color_mapper, map_handle,geosource)
+    
+    save(p,filename = pSet["htmlOut"],title = pSet["figTitle" ])
 
-
-
-              
-""",
-("Name","@NAMELSAD"),
-("Latitude", "@INTPTLAT")], mode='vline'
-              )
-"""
+if __name__ == "__main__":
+   main_mapper(settingSrc)
